64dc09d28b60a2f7677ade33bcb519ec4ca252dc
Kim Min Jeong <minjeong9919@naver.com>
2024-03-08 19:07:17 +0900

    Feat(#1): 카메라 프레임 구현 및 페이지 연동

---
+++ src/component/CameraIcon.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./cameraIcon.css";
+
+export const CameraIcon = () => {
+  const navigation = useNavigate();
+
+  const menuButtons = [
+    { id: 1, name: "Profile", route: "profile" },
+    { id: 2, name: "Career", route: "crofile" },
+    { id: 3, name: "Project", route: "project" },
+    { id: 4, name: "Skill", route: "skill" },
+  ];
+
+  const Button = ({ buttonName, route, size = "small" }) => {
+    const [isMenuButtonHover, setIsMenuButtonHover] = useState(false);
+    const defaultWidth = size === "big" ? "w-40" : "w-20";
+    const defaultHeight = size === "big" ? "h-40" : "h-20";
+
+    return (
+      <button
+        className={`${defaultHeight} ${
+          isMenuButtonHover ? "hover:w-full" : defaultWidth
+        } rounded-full bg-gray-300 p-4 ${size === "big" && "mt-8"}`}
+        onMouseEnter={() => setIsMenuButtonHover(true)}
+        onMouseLeave={() => setIsMenuButtonHover(false)}
+        onClick={() => navigation(`/${route}`)}
+      >
+        {size === "big" ? (
+          <div className="h-full w-full rounded-full bg-gray-950 flex items-center justify-center">
+            <span className="text-3xl font-black text-white">
+              {isMenuButtonHover && buttonName}
+            </span>
+          </div>
+        ) : (
+          <span className="text-3xl font-black">
+            {isMenuButtonHover && buttonName}
+          </span>
+        )}
+      </button>
+    );
+  };
+
+  return (
+    <>
+      <div className="relative h-p160 w-w400 rounded-2xl bg-gray-950 ml-xl px-24 py-12">
+        <div className="relative  h-p80 w-full z-40 rounded-2xl bg-gray-300"></div>
+        <div className="absolute h-p800 w-w1360 rounded-100px bg-gray-950 top-20 left-[-10rem] p-20 flex flex-row">
+          <div className="h-full w-full rounded-3xl bg-gray-300 mr-12"></div>
+          <div className="h-full w-3/12 rounded-3xl flex flex-col gap-8">
+            {menuButtons.map((button) => (
+              <Button
+                key={button.id}
+                buttonName={button.name}
+                route={button.route}
+              />
+            ))}
+            <Button buttonName="Capture Me!" size="big" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+import { CameraIcon } from "./../component/CameraIcon";
+
+const Home = () => {
+  return (
+    <div className="w-full h-lvh bg-zinc-400 pt-14">
+      <p className="text-5xl font-black ml-l mb-28 tracking-tighter">
+        안녕하세요. 프론트엔드 개발자 김민정입니다.
+      </p>
+      <CameraIcon />
+    </div>
+  );
+};
+
+export default Home;
