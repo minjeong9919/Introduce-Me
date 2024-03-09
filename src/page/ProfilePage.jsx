7b0966a41e2a9ff420a644ac65e13d3d9757cbf9
Kim Min Jeong <minjeong9919@naver.com>
2024-03-09 19:21:28 +0900

    Feat: Profile 페이지에 사진 컴포넌트 적용 및  디자인 추가

--- src/page/ProfilePage.jsx
+++ src/page/ProfilePage.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Profile } from "./../component/Pictures/Profile";
+import { profileContents } from "../constants/profileContents";
 
 export const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="w-full h-lvh bg-zinc-400 py-14 overflow-auto">
+      <div className="w-2/5 h-full bg-gray-950 mx-auto p-4 pb-40 grid grid-rows-2 grid-cols-2 gap-4">
+        {profileContents.map((profileContent) => (
+          <div key={profileContent.id} className="w-full bg-white">
+            <Profile
+              title={profileContent.title}
+              content={profileContent.content}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
