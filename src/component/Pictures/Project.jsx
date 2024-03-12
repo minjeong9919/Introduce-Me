93c692289636ebaa53e36cccf213300966ce80ca
Kim Min Jeong <minjeong9919@naver.com>
2024-03-12 19:10:23 +0900

    Design: 카드 확대 애니메이션 추가

--- src/component/Pictures/Project.jsx
+++ src/component/Pictures/Project.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const Project = () => {
+export const Project = ({ isModalVisible, setIsModalVisible }) => {
   return (
-    <div className="w-full h-fit bg-gray-950 mx-auto p-8 pb-10 rounded-lg relative">
-      <div className="bg-amber-900 w-8 h-20 absolute top-[-30px] inset-x-2/4"></div>
+    <div
+      className="w-full h-fit bg-gray-950 mx-auto p-8 pb-10 rounded-lg relative shadow-2xl shadow-black transition ease duration-500 hover:scale-105"
+      onClick={() => setIsModalVisible(!isModalVisible)}
+    >
+      <div className="bg-amber-900 w-8 h-20 absolute top-[-30px] inset-x-2/4 rounded-xl"></div>
       <div>
         <div className="w-full h-p500 bg-white rounded-lg mb-10">
           프로젝트 이미지
--- src/page/ProjectPage.jsx
+++ src/page/ProjectPage.jsx
@@ -3,7 +3,7 @@ import { Project } from "./../component/Pictures/Project";
 
 export const ProjectPage = () => {
   return (
-    <div className="w-full h-lvh bg-zinc-400 overflow-auto px-20 py-20">
+    <div className="w-full h-lvh bg-zinc-200 overflow-auto px-20 py-20">
       <p className="text-center text-7xl my-8 mb-20">Projects</p>
       <div className="w-4/5 grid grid-cols-3 gap-20 mx-auto">
         <Project />
