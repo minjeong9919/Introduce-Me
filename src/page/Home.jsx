7bbc1a7e9b24b0642cfafc02fb70d5eab8ebb915
Kim Min Jeong <minjeong9919@naver.com>
2024-03-14 15:56:34 +0900

    Refactor: 타이핑 효과 적용 수정

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,28 +1,17 @@
-import { useEffect, useState } from "react";
 import { CameraIcon } from "./../component/home/CameraIcon";
-// import Typewriter from "typewriter-effect";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
-  const IntroduceText = "안녕하세요. 프론트엔드 개발자 김민정입니다.";
-  const [text, setText] = useState("");
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setText(text + IntroduceText[count]);
-      setCount(count + 1);
-    }, 100);
-    if (count === IntroduceText.length) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  });
-
   return (
-    <div className="w-full h-lvh bg-zinc-400 pt-20">
-      <h1 className="text-5xl ml-l mb-20 Black Han Sans tracking-widest">
-        {text}
-      </h1>
+    <div className="w-full h-lvh overflow-auto bg-zinc-400 pt-16">
+      <Typewriter
+        options={{
+          strings: ["안녕하세요. 프론트엔드 개발자 김민정입니다."],
+          autoStart: true,
+          loop: true,
+          wrapperClassName: "text-5xl ml-l mb-28 tracking-widest",
+        }}
+      />
       <CameraIcon />
     </div>
   );
